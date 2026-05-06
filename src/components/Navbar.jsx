1a53b88137595cc95312b30198ad65eb32967107
ahm-fahim <contactahmfahim@gmail.com>
2026-05-06 16:49:23 +0600

    feat:add responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import "react";
 import { Link, NavLink } from "react-router-dom";
 import {assets }from "../assets/frontend_assets/assets";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [visible, setVisible] = useState(false);
+
     return <div className="flex items-center justify-between py-5 font-medium">
         <div className="text-xl font-bold">My <span className="text-orange-600">Com</span>fort</div>
         <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
@@ -30,6 +32,25 @@ const Navbar = () => {
                 <img src={assets.cart_icon} alt="Cart" className="w-5 cursor-pointer" />
                 <p className="absolute -bottom-2 -right-2 bg-orange-600 text-white text-xs rounded-full h-5 p-1 flex items-center justify-center">0</p>
             </Link>
+
+            <img onClick={()=>setVisible(true)} src={assets.menu_icon} alt="Menu" className="w-5 cursor-pointer sm:hidden" />
+        </div>
+
+        {/* Sidebar menu for small screen */}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white/90 transition-all ${visible ? 'w-full' : 'w-0'}`}>
+            <div onClick={()=>setVisible(false)} className="flex flex-col text-gray-600">
+                <div className="flex items-center justify-between px-2">
+                    <div className="flex items-center gap-4 p-3 cursor-pointer">
+                        <img className="h-4 rotate-180" src={assets.dropdown_icon} alt="" />
+                        <p>Back</p>
+                    </div>
+                    <h2 className="font-bold items-center">MY <span className="text-orange-600">COM</span>FORT</h2>
+                </div>
+                <NavLink to ="/" className="p-3 border-t border-gray-300">Home</NavLink>
+                <NavLink to ="/collections" className="p-3 border-t border-gray-300">Collections</NavLink>
+                <NavLink to ="/about" className="p-3 border-t border-gray-300">About</NavLink>
+                <NavLink to ="/contact" className="p-3 border-t border-gray-300">Contact</NavLink>
+            </div>
         </div>
   </div>;
 };
