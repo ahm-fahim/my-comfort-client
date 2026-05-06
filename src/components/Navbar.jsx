d8c9247d59a62cc9a28832eb9c27d1eb5e63d8e0
ahm-fahim <contactahmfahim@gmail.com>
2026-05-06 16:12:51 +0600

    feat:add basic nabar with all navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import "react";
-import { NavLink } from "react-router-dom";
-// import assets from "../assets/frontend_assets/assets";
+import { Link, NavLink } from "react-router-dom";
+import {assets }from "../assets/frontend_assets/assets";
 
 const Navbar = () => {
     return <div className="flex items-center justify-between py-5 font-medium">
@@ -13,8 +13,23 @@ const Navbar = () => {
         </ul>
 
         <div className="flex items-center gap-6">
-            {/* <img src={assets.search_icon} alt="Search"  className="w-5 cursor-pointer"/> */}
-            {/* <img src={assets.cart_icon} alt="Cart" /> */}
+            <img src={assets.search_icon} alt="Search" className="w-5 cursor-pointer" />
+
+            <div className="group relative">
+                <img src={assets.profile_icon} alt="Profile" className="w-5 cursor-pointer" />
+                <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+                    <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded-md">
+                        <p className="cursor-pointer hover:text-black">My Profile</p>
+                        <p className="cursor-pointer hover:text-black">Orders</p>
+                        <p className="cursor-pointer hover:text-black">Logout</p>
+                    </div>
+                </div>
+            </div>
+            
+            <Link to="/cart" className="relative">
+                <img src={assets.cart_icon} alt="Cart" className="w-5 cursor-pointer" />
+                <p className="absolute -bottom-2 -right-2 bg-orange-600 text-white text-xs rounded-full h-5 p-1 flex items-center justify-center">0</p>
+            </Link>
         </div>
   </div>;
 };
