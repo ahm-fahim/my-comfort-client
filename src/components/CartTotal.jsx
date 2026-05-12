72b064a583a4fb3e68c11a7bc18a2cf110f62ea1
ahm-fahim <contactahmfahim@gmail.com>
2026-05-12 11:58:53 +0600

    feat: cart total amount count function added

---
+++ src/components/CartTotal.jsx
@@ -0,0 +1,36 @@
+import { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
+
+const CartTotal = () => {
+    const { currency, deliveryFee, getCartAmount } = useContext(ShopContext);
+    console.log(getCartAmount());
+
+    
+    return (
+        <div className='w-full'>
+            <div className="text-2xl">
+                <Title text1={'CART'} text2={'TOTALS'}/>
+            </div>
+            <div className='flex flex-col mt-2 gap-2 text-sm'>
+                <div className="flex justify-between">
+                    <p>Subtotal</p>
+                    <p>{getCartAmount()}.00 {currency} </p>
+                </div>
+                <hr  className='text-gray-300'/>
+                <div className='flex justify-between'>
+                    <p>Shipping Fee</p>
+                    <p>{deliveryFee}.00 {currency}</p>
+                </div>
+                <hr  className='text-gray-300'/>
+                <div className='flex justify-between'>
+                    <b>Shipping Fee</b>
+                    <b>{getCartAmount() === 0 ? 0 : getCartAmount() + deliveryFee }.00 {currency}</b>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default CartTotal;
