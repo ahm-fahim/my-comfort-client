a3130adcee142a50e0bdb4f7973131535607afda
ahm-fahim <contactahmfahim@gmail.com>
2026-05-14 20:40:58 +0600

    feat: orders page ui updated

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const { products, currency } = useContext(ShopContext);
+
+  return <div className="border-t border-gray-300 pt-16">
+    <div className="text-2xl">
+      <Title text1={'MY'} text2={'ORDERS'} />
+    </div>
+    <div>
+      {
+        products.slice(1, 4).map((item, index) => (
+          <div key={index} className="py-4 border-t border-b border-gray-300 flex flex-col md:flex-row md:items-center md:justify-between gap-4 text-gray-700">
+            <div className="flex items-start gap-6 text-sm">
+              <img className="w-16 sm:w-20" src={item.image[0]} alt="" />
+              <div>
+                <p className="sm:text-base  font-medium">{item.name}</p>
+                <div className="flex items-center text-base gap-3 mt-2 text-gray-700">
+                  <p className="text-lg">{item.price}.00 {currency}</p>
+                  <p >Quantity : 1</p>
+                  <p>Size : XL</p>
+                </div>
+                <p className="mt-2"> Date : <span className="text-gray-400">14 May, 2026</span></p>
+              </div>
+            </div>
+            <div className="md:w-1/2 flex justify-between">
+              <div className="flex items-center gap-2">
+                <p className="min-w-2 h-2 rounded-full bg-green-500"></p>
+                <p className="text-sm md:text-base">Read to ship</p>
+              </div>
+              <button className="border px-4 py-2 border-gray-300 font-medium rounded-sm ">Track Order</button>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+
+  </div>;
 };
 
 export default Orders;
